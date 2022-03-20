dc73a1cc3634077b566b388687ff9353549134ef
Lattice0 <>
2022-03-20 19:57:20 -0300

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+Simple No-IP updater written in python3 that updates your machine IP in the server
+
+Just edit with your noip domain in the form 'mydomain.no-ip.org' or whatever TLD you used,
+then put your username and password and run. It will update your IP with the
+machine's current IP (the IP of the modem if you're behind a gateay).
+
+It'll use HTTPS :)
+
+# 'Installation'
+
+Just run
+
+```
+crontab -e
+```
+and write this line to run every minute:
+
+```
+* * * * * python3 /home/user/SimpleNoIPUpdater/noipupdater.py
+```
---
+++ noipupdater.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#simple noip HTTPS updater
+
+import time
+from http.client import HTTPSConnection
+from base64 import b64encode
+
+#User customizable part *********
+interval = 5 #in seconds
+noIpDomain = 'mydomain.no-ip.org'
+user = 'myusername@email.com'
+password = 'myPassword'
+n = 3 # number of update tries
+printSuccess = False #makes it print the success message in every try
+#end ****************************
+
+
+UserAndPasswordInBytes = (user+":"+password).encode("ascii")#convert to bytes
+
+userAndPassEncoded = b64encode(UserAndPasswordInBytes).decode("ascii")
+headers = { 'Authorization' : 'Basic %s' %  userAndPassEncoded }
+
+for x in range(0,n):
+    try:
+        c = HTTPSConnection("dynupdate.no-ip.com")
+        c.request('GET', '/nic/update?hostname='+noIpDomain, headers=headers)
+        res = c.getresponse()
+        data = res.read()
+        if "nochg" in data.decode("utf-8"):
+            if printSuccess:
+                print(data)
+            break;
+    except:
+        print('error')
+    time.sleep(interval)
+
